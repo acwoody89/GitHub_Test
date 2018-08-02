4085cf4acf010199c3edba5683961d693d8b7144
Kaelan <kadokaelan@gmail.com>
2018-08-02 11:57:56 -0400

    fixed conflict part 2

--- src/MergeConflict.java
+++ src/MergeConflict.java
@@ -1,16 +1,10 @@
-
 public class MergeConflict {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello moon");
-		System.out.println("My name is Alex.");
-		
-
-		System.out.println("And my name is KAELAN the great!!!!");
-
-		System.out.println("And my name is KAELAN shmaelan!!!!");
-
-	}
-
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello moon");
+        System.out.println("My name is Alex.");
+        
+        System.out.println("And my name is KAELAN the great!!!!");
+        System.out.println("And my name is KAELAN shmaelan!!!!");
+    }
 }
