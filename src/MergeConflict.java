755b5a661e2bcf271e90fb7742850cbbe0c39c01
Alex <acwoody89@gmail.com>
2018-08-02 11:45:04 -0400

    Break

--- src/MergeConflict.java
+++ src/MergeConflict.java
@@ -3,10 +3,10 @@ public class MergeConflict {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hello moon");
 		System.out.println("My name is Alex.");
 		
-		System.out.println("And my name is KAELAN!!!!");
+		System.out.println("And my name is KAELAN shmaelan!!!!");
 	}
 
 }
